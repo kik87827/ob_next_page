15d31431e44b499aa6d9d19bde0dd6d4a9c7fdc3
in kyung <kik87827@gmail.com>
2026-03-20 18:24:28 +0900

    프리패칭 스터디중

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,20 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const onClickButton = () => {
     router.push("/test");
+    // router.replace("/test");
+    // router.back("/test");
   };
+
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <header style={{ display: "flex", gap: 10 }}>
